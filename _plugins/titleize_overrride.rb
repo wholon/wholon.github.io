ad9a1523b6127e8f49a504bee7f25454e9d94926
Holon <holon.cn@gmail.com>
2022-04-18 11:29:46 +0800

    更新文章

--- _plugins/titleize_overrride.rb
+++ _plugins/titleize_overrride.rb
@@ -3,10 +3,10 @@
 # 有了这个插件，标题会保持原样，即 HTML基础
 # 详情见这篇文章： In Jekyll, how to change the capitalization rules for converting a file name into a blog post title?
 # 链接：https://stackoverflow.com/questions/37975716/in-jekyll-how-to-change-the-capitalization-rules-for-converting-a-file-name-int
-module Jekyll
-    module Utils
-        def titleize_slug(slug)
-        slug.split("-").join(" ")
-        end
-    end
-end
+# module Jekyll
+#     module Utils
+#         def titleize_slug(slug)
+#         slug.split("-").join(" ")
+#         end
+#     end
+# end
