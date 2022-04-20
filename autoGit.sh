dd4cf8490bef02ce85665f3e2b5c1988ff7d56d8
Holon <holon.cn@gmail.com>
2022-04-21 03:56:12 +0800

    更新文章

--- autoGit.sh
+++ autoGit.sh
@@ -8,24 +8,21 @@ cd $SHELL_FOLDER
 git checkout main
 
 # 删除 _site网站下所有内容
-rm -rf _site/*
+rm -rf _site
 
 # 从远程 clone gh-pages分支
 git clone -b gh-pages https://github.com/wholon/wholon.github.io.git _site
 # 以生产模式 build 网站
 JEKYLL_ENV=production bundle exec jekyll build
+# 推送源代码
+git add .
+# 变量1是传送的参数，也就是本次 git 的摘要
+git commit -m "$1" 
+git push origin
 
 # 推送编译好的博客
 cd _site
 echo "[主页](https://holon.wang)" > README.md
 git add .
 git commit -m "$1"
-git push origin
-
-# 推送源代码
-cd ..
-git checkout main
-git add .
-# 变量1是传送的参数，也就是本次 git 的摘要
-git commit -m "$1" 
 git push origin
