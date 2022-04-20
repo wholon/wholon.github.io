3b31916b25620c27e33f83b0105df674ac077871
Holon <holon.cn@gmail.com>
2022-04-20 22:28:15 +0800

    更新文章

--- autoGit.sh
+++ autoGit.sh
@@ -5,7 +5,7 @@ SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 cd $SHELL_FOLDER
 
 # 以生产模式 build 网站
-JEKYLL_ENV=production bundle exec jekyll build
+JEKYLL_ENV=production bundle exec jekyll build --incremental
 git add .
 # 变量1是传送的参数，也就是本次 git 的摘要
 git commit -m "$1" 
